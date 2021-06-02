c9f359e70180c6a10d6f2889b61d8d2130f0b000
Bradley Marsh <bradley.marsh@developyn.com>
2021-06-02 19:09:38 +0100

    map for chatcards

--- front/src/components/Chat/ChatMenu.jsx
+++ front/src/components/Chat/ChatMenu.jsx
@@ -2,13 +2,28 @@ import ChatHeader from './ChatHeader';
 import SearchBar from './SearchBar';
 import ChatCard from './ChatCard';
 
+const chatCards = [
+  {
+    userName: "Brad Marsh",
+    tagline: "last online 2 minutes"
+  },
+  {
+    userName: "Brad Marsh",
+    tagline: "last online 2 minutes"
+  },
+  {
+    userName: "Brad Marsh",
+    tagline: "last online 2 minutes"
+  },
+]
+
 export default function ChatMenu() {
   return (
     <div className="flex flex-col w-96" style={{ width: '34rem'}}>
       <ChatHeader />
       <SearchBar />
       <div className="w-full mt-4">
-        <ChatCard userName="Brad Marsh" tagline="last online 2 minutes" />
+        { chatCards.map(card => <ChatCard userName={card.userName} tagline={card.tagline} />) }
       </div>
     </div>
   )
